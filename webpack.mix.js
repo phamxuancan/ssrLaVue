97c240bda94eed6470fa8552960e07115dcf2bfc
canpx <canpx@vnext.com.vn>
2020-12-08 15:27:45 +0700

    ssr laravel vuejs

---
+++ webpack.mix.js
@@ -0,0 +1,25 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+	.js('resources/js/entry-client.js', 'public/js')
+	.js('resources/js/entry-server.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');
+
+mix.webpackConfig({
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.runtime.common.js'
+    }
+  }
+});
