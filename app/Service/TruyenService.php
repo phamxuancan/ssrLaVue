9c8b79612486f4053ce4bcaeacd9595f535429e9
thuhuyen <thuhuyen@gmail.com>
2021-01-12 22:11:13 +0700

    add header

--- app/Service/TruyenService.php
+++ app/Service/TruyenService.php
@@ -17,11 +17,11 @@ class TruyenService
             $total_truyen = Truyen::count();
         $total_theloai = DSTheLoai::count();
         $url = "https://truyenfull.vn/danh-sach/truyen-hot/";
-        // $headers = array();
+         $headers = array();
 		// $headers[] = 'Host: truyenfull.vn';
-		// $headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0';
-		// $headers[] = 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8';
-		// $headers[] = 'Accept-Language: en-US,en;q=0.5';
+		 $headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0';
+		 $headers[] = 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8';
+		 $headers[] = 'Accept-Language: en-US,en;q=0.5';
 		// $headers[] = 'Referer: http://truyenfull.vn/the-loai/sac/';
 		//$headers[] = 'Cookie: _ga=GA1.2.811952154.1451875061; history-cookie=KzvPvbVsOibiBLaHV5bmggVGhpw6puIEtodXnhur90OnZhbi1raHV5bmgtdGhpZW4ta2h1eWV0OjI6NDN8VGjhuqduIEto4buRbmcgVGhpw6puIEjhuqE6dHJ1eWVuLXRoYW4ta2hvbmctdGhpZW4taGE6OjI2NzM%3D; _gat=1';
 		$headers[] = 'Connection: keep-alive';
